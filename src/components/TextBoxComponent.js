a7d9e8fc7ca652ab17fb4329e1c539e8e42aadde
Damon <ddhenry95@gmail.com>
2021-09-24 19:08:30 -0700

    Help?

--- src/components/TextBoxComponent.js
+++ src/components/TextBoxComponent.js
@@ -1,37 +1,62 @@
 import React, { Component } from 'react'
 import { Button } from 'reactstrap'
 
-// Spreadsheets
+// Check line 41 for a comment on my current situation.
+
+const onPostClick = () => {
+    console.log(`Post clicked!`)
+}
 
 const PostButton = () => {
+
     return(
         <div className='row justify-content-center'>
-            <Button onClick={() => console.log(`Post button clicked!`)} color='success' size='lg'>Post</Button>
+            <Button onClick={onPostClick()} color='success' size='lg'>Post</Button>
         </div>
     )
 }
 
-const TextInput = () => {
-    return(
-        <div className='row justify-content-center'>
-            <textarea
-                className='col form-control mt-4'
-                placeholder={`What's on your mind?`}
-                style={{resize: 'none', height: '480px', paddingBottom: '20px'}}
-            >
-            </textarea>
-            <Button onClick={() => console.log(`Submit button clicked!`)} color='primary'>Submit</Button>
-        </div>
-    )
+const TextInput = (postClicked) => {
+
+    if (postClicked) {
+        return(
+            <div className='row justify-content-center'>
+                <textarea
+                    className='col form-control mt-4'
+                    placeholder={`What's on your mind?`}
+                    style={{resize: 'none', height: '480px', paddingBottom: '20px'}}
+                >
+                </textarea>
+                <Button onClick={() => console.log(`Submit button clicked!`)} color='primary'>Submit</Button>
+            </div>
+        )
+    } else {
+        return (
+            <div>Click the button to proceed!</div>
+        )
+    }
+
 }
+
+/* I think by passing [postClick], I can check to make sure it reads
+whether or not it's true, and that should help me toggle the text input.
+I feel like I'm approaching this wrong because the text box is being
+displayed without the button being clicked. */
 
 class TextBox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            postClick: false,
+            submitClick: false
+        }
+    }
 
     render() {
         return (
             <div className='container mt-4 mx-auto'>
                 <PostButton />
-                <TextInput />
+                <TextInput postClicked={this.state.postClick}/>
             </div>
         )
     }
